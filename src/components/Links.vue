ec59ced9e7b874ffd829454a0f8d91fd2456fddf
morimolymoly <morimolymoly@gmail.com>
2018-05-01 23:43:46 +0900

    updated link

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -4,6 +4,7 @@
     <ul>
       <li><a href="http://morimolymoly.hateblo.jp/">Blog</a></li>
       <li><a href="https://github.com/moly333">GitHub</a></li>
+      <li><a href="https://moly333.github.io/nikki/">日記</a></li>
     </ul>
     <PageFooter></PageFooter>
   </div>
